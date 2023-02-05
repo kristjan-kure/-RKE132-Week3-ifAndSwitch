e755b43a59f11b2ddbf4b210fce077aac309efff
kristjan-kure <kristjan.kure.1@gmail.com>
2023-02-05 19:08:59 +0200

    add week3 code

---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,14 @@
+﻿//modulus, symbol: %
+
+int a = 24;
+int b = 8;
+int c = b % a; //8. 8 cant be divided into 24 parts. All is kept as it is.
+Console.WriteLine(c);
+
+int d = a % b; //0. 24 can be divided with 8. No modulus left.
+Console.WriteLine(d);
+
+a = 23;
+int e = a % b; //7. 8 * 2 = 16; 23 - 16 = 7
+Console.WriteLine(e);
+
